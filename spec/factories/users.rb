37936069db9fbf7e728ac00799c43ef9f179d8e1
nakai-hiroto <ichika077@gmail.com>
2023-03-30 12:39:16 +0900

    テストコードの検証対象に対する記述

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {Faker::Name.initials(number: 2)}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    last_name             {"山本"}
+    first_name            {"太郎"}
+    last_name_kana        {"ヤマモト"}
+    first_name_kana       {"タロウ"}        
+    birthday              {"1900-01-31"}
+  end
+end
