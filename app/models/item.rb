2a330b7adb0292377d72e1f41c98d9ba8211469a
nakai-hiroto <ichika077@gmail.com>
2023-03-31 21:03:18 +0900

    itemモデルに記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,23 @@
 class Item < ApplicationRecord
-  
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
   has_one_attached :image
+
+  #ActiveHashを使用したファイルとのアソシエーション
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_payer
+  belongs_to :prefecture
+  belongs_to :shipping_day
+
+  validates :image, :item_name, :production, presence: true
+  validates :price,                  presence: true
+
+  #ジャンルの選択が「---」の時は保存できないようにする
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_payer_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_day_id, numericality: { other_than: 1 , message: "can't be blank"}
 
 end
