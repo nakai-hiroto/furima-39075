80866a22c3c3dad40c8c50348a45c7c13da35d98
nakai-hiroto <ichika077@gmail.com>
2023-03-31 14:20:27 +0900

    誤字の修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -79,7 +79,7 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
       end
-      it 'passwordは英字飲みでは登録できない' do
+      it 'passwordは英字のみでは登録できない' do
         @user.password = 'aaaaaa'
         @user.valid?
         expect(@user.errors.full_messages).to include('Password には英字と数字の両方を含めて設定してください')
