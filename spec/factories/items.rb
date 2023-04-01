8d936f11b7e7ff5340bc48e9484cdb8dcf3a19a7
nakai-hiroto <ichika077@gmail.com>
2023-04-01 17:21:21 +0900

    itemmodelのテストコードの記述

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,20 @@
 FactoryBot.define do
   factory :item do
+    association :user
+
+    item_name           {'オムライス'}
+    production          {'美味しいよ'}
+    category_id         {2}
+    condition_id        {2}
+    shipping_payer_id   {2}
+    prefecture_id       {2}
+    shipping_day_id     {2}
+    price               {500}
+
+    
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
     
   end
 end
