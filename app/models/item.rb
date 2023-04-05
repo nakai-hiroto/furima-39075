404b7cb41a26b74e8fb50254a20767f4aa4eb85d
nakai-hiroto <ichika077@gmail.com>
2023-04-05 19:49:35 +0900

    アソシエーションの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,7 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
   has_one_attached :image
+  has_one :purchase
 
   # ActiveHashを使用したファイルとのアソシエーション
   belongs_to :category
