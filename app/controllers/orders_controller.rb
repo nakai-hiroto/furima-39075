a38afd707c7ba9c9594cdcddb53de70a09873431
nakai-hiroto <ichika077@gmail.com>
2023-04-06 19:11:08 +0900

    orderscontrollerにtokenの記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,5 @@
 class OrdersController < ApplicationController
   def index
-    #@purchase_address = PurchaseAddress.find(params[:item_id])
     @item = Item.find(params[:item_id])
     @purchase_address = PurchaseAddress.new
   end
@@ -9,6 +8,7 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
     @purchase_address = PurchaseAddress.new(purchase_address_params)
     if @purchase_address.valid?
+       pay_item
        @purchase_address.save
         redirect_to root_path 
     else
@@ -19,8 +19,17 @@ class OrdersController < ApplicationController
   private
 
   def purchase_address_params
-    params.require(:purchase_address).permit(:post_code, :prefecture_id, :city, :street_address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:purchase_address).permit(:post_code, :prefecture_id, :city, :street_address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
     
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,  # 商品の値段
+      card: purchase_address_params[:token],    # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+    )
   end
 
 end
