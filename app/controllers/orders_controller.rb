94db1760ba6c5c03eda739e4170ce7687a3d21b8
nakai-hiroto <ichika077@gmail.com>
2023-04-07 09:56:46 +0900

    orders controllerに記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,11 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: [:index]
   def index
     @item = Item.find(params[:item_id])
     @purchase_address = PurchaseAddress.new
+    if @purchase_address.save
+      redirect_to root_path
+    end
   end
 
   def create
