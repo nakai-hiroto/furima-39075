d4383128801b172a9d259a4dda79048b4da2b2c1
nakai-hiroto <ichika077@gmail.com>
2023-04-07 09:55:06 +0900

    エラーメッセージを追加

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -8,7 +8,7 @@ class PurchaseAddress
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :city
   validates :street_address
-  validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'Input only number' }
+  validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "Input only number" }
   validates :user_id
   validates :item_id
   validates :token
